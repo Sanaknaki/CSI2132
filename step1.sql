01e52fbb6fe726920a09fb7a6af57092ac509269
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-02-19 22:13:26 -0500

    step1.sql init

---
+++ step1.sql
@@ -0,0 +1,71 @@
+-- Select 
+SELECT 
+INSERT
+DELETE
+UPDATE
+
+
+-- Student applied to a job
+INSERT INTO jobs_applied (student_id, Job_id)
+VALUES ( :student_id, :job_id)
+
+-- Add a student
+INSERT INTO Students (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- Add a user
+INSERT INTO User (email, username, password)
+VALUES (:email, :username, :password)
+
+-- Add a Moderator
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- Add program
+INSERT INTO Program (program_name, required_number_of_credits, university_id)
+VALUES (:pname, :rcredits, :uni_id)
+
+-- add university
+INSERT INTO University (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add program enrolled
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add upvote
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add resume
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add resumeReview
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add interview
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add jobs
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add company
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add grades
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- add courses
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
+-- jobs_applied
+INSERT INTO Moderator (first_name, last_name, user_id)
+VALUES (:fname, :lname, :user_id)
+
