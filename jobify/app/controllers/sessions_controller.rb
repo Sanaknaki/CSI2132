a19e24a0f9961968d71a4f21c62822243cae5427
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-27 14:38:00 -0400

    new function

--- jobify/app/controllers/sessions_controller.rb
+++ jobify/app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save the user id inside the browser cookie. This is how we keep the user 
+      # logged in when they navigate around our website.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user's login doesn't work, send them back to the login form.
+      redirect_to '/login'
+  end
 
   def create; end
 
