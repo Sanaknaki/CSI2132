38aa07ceff6a6f10e237c119764ab63e20147240
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-27 14:38:22 -0400

    destory function

--- jobify/app/controllers/sessions_controller.rb
+++ jobify/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
   def new
+    user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
@@ -10,9 +11,13 @@ class SessionsController < ApplicationController
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
+    end
   end
 
   def create; end
 
-  def destory; end
+  def destory
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
 end
