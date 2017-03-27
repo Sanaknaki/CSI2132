f27a870409ceef417cf6c11f301db290d01b3410
Ali Sanaknaki <asana085@uottawa.ca>
2017-03-27 14:51:21 -0400

    Front-End shinanigans

--- jobify/app/controllers/pages_controller.rb
+++ jobify/app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
   def home
     render('index')
   end
+
+  def about
+    render('about')
+  end
+
+  def tos
+    render('tos')
+  end
+
+  def main
+    render('main')
+  end
 end
