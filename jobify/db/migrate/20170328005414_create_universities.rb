e4115ca219ef1bcdc9e823592f249e66b1f3f5a8
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-27 20:54:29 -0400

    fixing spelling mistake

--- jobify/db/migrate/20170328005414_create_universities.rb
+++ jobify/db/migrate/20170328005414_create_universities.rb
@@ -1,7 +1,7 @@
 class CreateUniversities < ActiveRecord::Migration[5.0]
   def change
     create_table :universities do |t|
-      t.stirng :name
+      t.string :name
       t.string :address
 
       t.timestamps
