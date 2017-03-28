920fcdd9671d119f804f66a48cf32e3ff415cab6
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-27 20:54:49 -0400

    db migrate

--- jobify/db/schema.rb
+++ jobify/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328005117) do
+ActiveRecord::Schema.define(version: 20170328005414) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -83,6 +83,13 @@ ActiveRecord::Schema.define(version: 20170328005117) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "universities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
