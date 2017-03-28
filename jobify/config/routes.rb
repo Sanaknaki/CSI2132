00dde18c9055b1db21785cf7390fc4e4ed4cf8e5
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-28 12:58:44 -0400

    added a new route

--- jobify/config/routes.rb
+++ jobify/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Pages
   get '/', to: 'pages#home'
-  get '/about' => 'pages#about'
-  get '/tos' => 'pages#tos'
   get '/main' => 'pages#main'
   # Sessions
   get    '/login',   to: 'sessions#new'
