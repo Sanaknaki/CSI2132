6cbbb128d9a890a904297d56ffe8fa3d5d6645d0
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-28 20:16:14 -0400

    redirect to main page if logged in

--- jobify/app/controllers/sessions_controller.rb
+++ jobify/app/controllers/sessions_controller.rb
@@ -2,19 +2,24 @@
 class SessionsController < ApplicationController
   before_filter :authenticate_user, :except => [:new, :create, :destroy]
 
-  def new; end
+  def new
+    unless session[:user_id] || (User.find_by_id(session[:user_id]))
+      render 'new'
+    else
+      redirect_to '/'
+    end
+  end
 
   def create
     @user = User.find_by_email(params[:email].downcase)
     # If the user exists AND the password entered is correct.
-    puts session[:user_id]
     if @user && @user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = @user.id
       redirect_to '/'
     else
-    # If user's login doesn't work, send them back to the login form.
+      # If user's login doesn't work, send them back to the login form.
       render 'new'
     end
   end
