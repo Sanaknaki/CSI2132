7ea95b3bc066f9408a2f39ad541c72f324c93703
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-31 13:58:02 -0400

    working on signup

--- jobify/app/controllers/users_controller.rb
+++ jobify/app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if params[:is_student]
+      @student = Student.new(person_params)
+    else if params[:is_moderator]
+      @moderator = Moderator.new(person_params)
+    end
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/'
@@ -27,6 +32,14 @@ class UsersController < ApplicationController
       :email,
       :password,
       :password_confirmation
+    )
+  end
+  private
+
+  def person_params
+    params.require(:person).permit(
+      :first_name,
+      :last_name
     )
   end
 end
