88f144fac1b6188ed1ff09a965984a87c9a9cf48
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-03-31 13:09:32 -0400

    resume controller to handle uploads

---
+++ jobify/app/controllers/resumes_controller.rb
@@ -0,0 +1,31 @@
+# Resume controller
+class ResumesController < ApplicationController
+  layout 'index_layout'
+  def create
+    # TODO add it to the database
+    @resume = Resume.create
+    uploaded_io = params[:resume_pdf]
+    File.open(Rails.root.join('public', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+  end
+
+  def index
+    @resumes = Resume.all
+    render('index')
+  end
+
+  def upload
+    @resume = Resume.create
+    render('upload')
+  end
+
+  private
+
+  # Use strong_parameters for attribute whitelisting
+  # Be sure to update your create() and update() controller methods.
+
+  def resume_params
+    params.require().permit(:resume_pdf)
+  end
+end
