56f053b8f82800d2a96e8f3d383727480113779e
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 16:57:57 -0400

    rake db migrate

--- jobify/db/schema.rb
+++ jobify/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401203329) do
+ActiveRecord::Schema.define(version: 20170401205511) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -86,8 +86,11 @@ ActiveRecord::Schema.define(version: 20170401203329) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "student_num"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "universities", force: :cascade do |t|
