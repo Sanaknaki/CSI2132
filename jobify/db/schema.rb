5acbe8cd29ca29097e281000b77b26e2755b918a
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 16:37:23 -0400

    schema

--- jobify/db/schema.rb
+++ jobify/db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331144944) do
+ActiveRecord::Schema.define(version: 20170401203329) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
     t.string   "sector"
     t.string   "rating"
     t.string   "website"
-    t.string   "colour"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -77,13 +75,12 @@ ActiveRecord::Schema.define(version: 20170331144944) do
   end
 
   create_table "resumes", force: :cascade do |t|
-    t.binary   "resume"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.string   "pdf_file_name"
-    t.string   "pdf_content_type"
-    t.integer  "pdf_file_size"
-    t.datetime "pdf_updated_at"
+    t.string   "resume_path"
+    t.integer  "version"
+    t.integer  "student_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["student_id"], name: "index_resumes_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
