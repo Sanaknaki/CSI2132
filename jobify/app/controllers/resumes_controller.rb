a192c0be02c77a1f90d711c7f2c3f6f0019b6bfc
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 16:53:19 -0400

    added resume to a student

--- jobify/app/controllers/resumes_controller.rb
+++ jobify/app/controllers/resumes_controller.rb
@@ -3,11 +3,16 @@ class ResumesController < ApplicationController
   layout 'index_layout'
   def create
     # TODO add it to the database
-    @resume = Resume.create
+    
     uploaded_io = params[:resume_pdf]
     File.open(Rails.root.join('public', uploaded_io.original_filename), 'wb') do |file|
       file.write(uploaded_io.read)
     end
+    ver = @student.resumes.last.id + 1
+    @resume = @student.resumes.create(
+      :resume_path => path,
+      :version => ver
+    )
     redirect_to '/'
   end
 
