5b5b52a48e8525f39611fba9287220b177d40964
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 11:41:05 -0400

    call resume uploader

--- jobify/app/controllers/users_controller.rb
+++ jobify/app/controllers/users_controller.rb
@@ -13,12 +13,16 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if params[:is_student]
       @student = Student.new(person_params)
-    else if params[:is_moderator]
+    elsif params[:is_moderator]
       @moderator = Moderator.new(person_params)
     end
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      if params[:is_student]
+        redirect_to :controller => 'resumes', :action => 'upload'
+      else
+        redirect_to '/'
+      end
     else
       render 'sessions/new'
     end
@@ -34,7 +38,6 @@ class UsersController < ApplicationController
       :password_confirmation
     )
   end
-  private
 
   def person_params
     params.require(:person).permit(
