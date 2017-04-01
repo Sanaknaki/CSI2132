98046729ce9f57ac6814352492d817ea0206c1bf
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 16:36:49 -0400

    recreating a model

--- jobify/db/migrate/20170401203329_create_resumes.rb
+++ jobify/db/migrate/20170401203329_create_resumes.rb
@@ -1,7 +1,9 @@
 class CreateResumes < ActiveRecord::Migration[5.0]
   def change
     create_table :resumes do |t|
-      t.binary :resume
+      t.string :resume_path
+      t.integer :version
+      t.references :student, foreign_key: true
 
       t.timestamps
     end
