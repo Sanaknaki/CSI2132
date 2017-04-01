8e06b6919f5a4e38eeb083b82229573efee56814
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 17:48:11 -0400

    handle uploading new resume

--- jobify/app/controllers/resumes_controller.rb
+++ jobify/app/controllers/resumes_controller.rb
@@ -5,8 +5,12 @@ class ResumesController < ApplicationController
     # TODO add it to the database
     
     uploaded_io = params[:resume_pdf]
-    @student = Student.last
-    ver = @student.resume.last ? @student.resume.last.id + 1 : 1
+    if session[:user_id]
+      @student = User.find_by_id(session[:user_id]).student
+    else
+      @student = Student.last
+    end
+    ver = @student.resume.last ? @student.resume.last.version + 1 : 1
     ext = /\.[a-zA-Z]+/.match(uploaded_io.original_filename).to_s
     path = @student.student_num.to_s + '_' + ver.to_s + ext
     File.open(Rails.root.join('public', path), 'wb') do |file|
@@ -26,25 +30,7 @@ class ResumesController < ApplicationController
   end
 
   def upload
-    @resume = Resume.create
     render('upload')
-  end
-
-  def new_version
-    uploaded_io = params[:resume_pdf]
-    @student = User.find_by_id(session[:user_id]).student
-    ver = @student.resume.last ? @student.resume.last.id + 1 : 1
-    ext = /\.[a-zA-Z]+/.match(uploaded_io.original_filename).to_s
-    path = @student.student_num.to_s + '_' + ver.to_s + ext
-    File.open(Rails.root.join('public', path), 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
-    @resume = @student.resume.create(
-      :resume_path => path,
-      :version => ver
-    )
-    @resume.save
-    redirect_to '/'
   end
 
   private
