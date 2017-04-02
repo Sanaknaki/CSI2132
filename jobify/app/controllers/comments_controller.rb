7fb152bcd396d8054ce722b9db421f0ea3a71f2f
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 23:04:28 -0400

    comments works

--- jobify/app/controllers/comments_controller.rb
+++ jobify/app/controllers/comments_controller.rb
@@ -1,43 +1,49 @@
-# comment controller
+# Comment controller
 class CommentsController < ApplicationController
-  
   def index
     render('index')
     @comments = Comment.all
   end
-  
+
   def fetch_comment
     @comment = Comment.find(params[:id])
     render('comment_details')
   end
-  
+
   def add_company_comment
-    @company = Company.find(params[:company_id])
-    @comment = @company.comments.create(comment_params)
-    if @comment.save
-      redirect_to @company
-    else
-      flash.now[:danger] = "error"
-    end
+    @company = Company.find(params[:id])
+    @comment = Comment.new(
+      :ctext => params[:comment][:ctext],
+      :company_id => params[:id]
+    )
+    @comment.save
+    redirect_to @company
   end
-  
+
   def add_job_comment
     @job = Job.find(params[:job_id])
     @job = @job.comments.create(comment_params)
     if @comment.save
       redirect_to @job
     else
-      flash.now[:danger] = "error"
+      flash.now[:danger] = 'error'
     end
   end
-  
+
   def add_resume_comment
     @resume = Resume.find(params[:resume_id])
     @resume = @resume.comments.create(comment_params)
     if @comment.save
       redirect_to @resume
     else
-      flash.now[:danger] = "error"
+      flash.now[:danger] = 'error'
     end
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:ctext)
+  end
+
 end
--- jobify/app/models/comment.rb
+++ jobify/app/models/comment.rb
@@ -1,5 +1,3 @@
 class Comment < ApplicationRecord
-  belongs_to :job
-  belongs_to :resume
   belongs_to :company
 end
