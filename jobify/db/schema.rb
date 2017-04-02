e251b4c48d17922e5e02bf98ca6b85f623577079
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-01 20:14:40 -0400

    db migrate

--- jobify/db/schema.rb
+++ jobify/db/schema.rb
@@ -14,15 +14,11 @@ ActiveRecord::Schema.define(version: 20170401235716) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "ctext"
+    t.boolean  "is_company"
+    t.boolean  "is_job"
+    t.boolean  "is_resume"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "comments_companies", id: false, force: :cascade do |t|
-    t.integer "comment_id", null: false
-    t.integer "company_id", null: false
-    t.index ["comment_id", "company_id"], name: "index_comments_companies_on_comment_id_and_company_id"
-    t.index ["company_id", "comment_id"], name: "index_comments_companies_on_company_id_and_comment_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -32,8 +28,6 @@ ActiveRecord::Schema.define(version: 20170401235716) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "ctext_id"
-    t.index ["ctext_id"], name: "index_companies_on_ctext_id"
   end
 
   create_table "courses", force: :cascade do |t|
