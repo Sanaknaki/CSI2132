cf8476da8fe382800a9f4a795757faf55fb7f4a6
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-03 19:51:06 -0400

    changed the radio button

--- jobify/app/controllers/users_controller.rb
+++ jobify/app/controllers/users_controller.rb
@@ -11,18 +11,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if params[:user] == 'student'
+    if params[:user_type] == 'student'
       @student = Student.create(student_params)
       @user.student = @student
       @student.save
-    elsif params[:user] == 'moderator'
+    elsif params[:user_type] == 'moderator'
       @moderator = Moderator.new(moderator_params)
       @moderator.save
       @user.moderator = @moderator
     end
     if @user.save
       session[:user_id] = @user.id
-      if params[:is_student]
+      if params[:user_type] == 'student'
         redirect_to :controller => 'resumes', :action => 'upload'
       else
         redirect_to '/'
