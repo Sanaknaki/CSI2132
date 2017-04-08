1a739cb24ee3815b4f7e3ec69947d232d937f2ea
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-08 00:03:29 -0400

    db migrate

--- jobify/db/schema.rb
+++ jobify/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408021545) do
+ActiveRecord::Schema.define(version: 20170408040044) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "ctext"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20170408021545) do
 
   create_table "job_comments", force: :cascade do |t|
     t.integer  "job_id"
+    t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_job_comments_on_job_id"
