c5b1e06f15fc7a6c83402434a090233fa3b30a4d
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-04-07 20:30:16 -0400

    403 if student

--- jobify/app/controllers/application_controller.rb
+++ jobify/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  NotAuthorized = Class.new(StandardError)
   def current_user
     begin
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -15,5 +15,23 @@ class ApplicationController < ActionController::Base
 
   def authorize
     redirect_to '/login' unless current_user
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render_error_page(status: 404, text: 'Not found')
+  end
+
+  rescue_from ApplicationController::NotAuthorized do |exception|
+    render_error_page(status: 403, text: 'Forbidden')
+  end
+
+  private
+
+  def render_error_page(status:, text:, template: 'errors')
+    respond_to do |format|
+      format.json { render json: {errors: [message: "#{status} #{text}"]}, status: status }
+      format.html { render template: "#{template}/#{status}", status: status, layout: false }
+      format.any  { head status }
+    end
   end
 end
